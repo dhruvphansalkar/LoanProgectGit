9b86174fa9f57aeb496b555999969b55f1460d5a
IG <ADM-IG-HWDLAB1B@capgemini.com>
2018-10-01 16:52:30 +0530

    cahnged ui

---
+++ src/com/cg/lms/ui/LadUi.java
@@ -0,0 +1,37 @@
+package com.cg.lms.ui;
+
+import java.util.Scanner;
+
+import com.cg.lms.exception.LoanException;
+import com.cg.lms.service.LoanManagementService;
+
+public class LadUi 
+{
+	LoanManagementService lService=null;
+	Scanner sc = new Scanner(System.in);
+	public void ladUiMethod()
+	{
+		System.out.println("Enter your login id:");
+		String uname=sc.next();
+		System.out.println("Enter your password:");
+		String pwd=sc.next();
+	
+			try {
+				int value = lService.login(uname,pwd);
+				if(value == -1)
+				{
+					System.out.println("Wrong Credentials.Please try again");
+				}
+				if(value == 1)
+				{
+					System.out.println("Congratulations");
+				}
+				
+			} 
+			catch (LoanException e)
+			{
+				e.printStackTrace();
+			}
+		
+	}
+}
