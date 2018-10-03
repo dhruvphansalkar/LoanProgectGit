f101e7368292f844721eab3be4d7077f7783261d
IG <ADM-IG-HWDLAB1B@capgemini.com>
2018-10-03 13:50:16 +0530

    update Pranav

--- src/com/cg/lms/ui/LadUi.java
+++ src/com/cg/lms/ui/LadUi.java
@@ -1,7 +1,11 @@
 package com.cg.lms.ui;
 
+import java.sql.Date;
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import com.cg.lms.bean.LoanApplication;
+import com.cg.lms.bean.LoanProgramsOffered;
 import com.cg.lms.exception.LoanException;
 import com.cg.lms.service.LoanManagementService;
 
@@ -9,6 +13,7 @@ public class LadUi
 {
 	LoanManagementService lService=null;
 	Scanner sc = new Scanner(System.in);
+	
 	public void ladUiMethod()
 	{
 		System.out.println("********LAD Login Page*********");
@@ -27,7 +32,45 @@ public class LadUi
 				}
 				if(value == 1)
 				{
-					System.out.println("Congratulations");
+					System.out.println("*****************Congratulations********************");
+				
+					
+					System.out.println("Welcome LAD");
+					System.out.println("Select Operation to be performed:");
+					System.out.println("1)view Loan Program Offered \n"
+							+ "2)update Application Status \n"
+							+ "3)set Status After Interview \n"
+							+ "4)view Application By Loan Program\n"
+							+ "5)EXIT ");
+					System.out.println("Enter your choice:");
+					int c=sc.nextInt();
+					do{
+					switch(c)
+						{
+							case 1:viewApplicationByLoanProgram();
+								break;
+							case 2:updateApplicationStatus();
+								break;
+							case 3:setStatusAfterInterview();
+								break;
+							case 4:viewLoanProgramOffered();
+								break;
+							case 5:
+								System.exit(0);
+							default:
+								System.out.println("Wrong Choice");
+						}
+					System.out.println("Welcome LAD");
+					System.out.println("Select Operation to be performed:");
+					System.out.println("1)view Application By Loan Program \n"
+							+ "2)update Application Status \n"
+							+ "3)set Status After Interview \n"
+							+ "4)view Loan Program Offered \n"
+							+ "5)EXIT ");
+					System.out.println("Enter your choice:");
+					c=sc.nextInt();
+					}while(c!=5);
+					
 				}
 				
 			} 
